35bb7bb88e33ddf65ffcc6fa5dc6dccb2fd426e5
MD_Codewright <30684432+MDCodewright@users.noreply.github.com>
2019-10-30 09:36:52 +0300

    Update tutorcourses.blade.php

--- smptutor/resources/views/tutorcourses.blade.php
+++ smptutor/resources/views/tutorcourses.blade.php
@@ -1,66 +1,55 @@
 @extends('layouts.modulesdash')
 
 @section('content')
-<section class="content-header">
-        <h1>
-            Modules
-            <small>Basic Plan</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
-    </section>
     <section class="content">
-    <div class="row">
-        <div class="col-md-10">
-          <div class="box box-default collapsed-box box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">Assignments</h3>
+      <div class="row">
+          <div class="col-md-10">
+            <div class="box box-default collapsed-box box-solid">
+              <div class="box-header with-border">
+                <h3 class="box-title">Assignments</h3>
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-                </button>
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                  </button>
+                </div>
+                <!-- /.box-tools -->
               </div>
-              <!-- /.box-tools -->
+              <!-- /.box-header -->
+              <div class="box-body">
+                <a href="#">Assignment 1</a>
+              </div>
+              <div class="box-body">
+                <a href="#">Assignment 2</a>
+              </div>
+              <!-- /.box-body -->
             </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <a href="#">Assignment 1</a>
-            </div>
-            <div class="box-body">
-              <a href="#">Assignment 2</a>
-            </div>
-            <!-- /.box-body -->
+            <!-- /.box -->
           </div>
-          <!-- /.box -->
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-10">
-          <div class="box box-default collapsed-box box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">Announcements</h3>
+      </div>
+      <div class="row">
+          <div class="col-md-10">
+            <div class="box box-default collapsed-box box-solid">
+              <div class="box-header with-border">
+                <h3 class="box-title">Announcements</h3>
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
-                </button>
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                  </button>
+                </div>
+                <!-- /.box-tools -->
               </div>
-              <!-- /.box-tools -->
+              <!-- /.box-header -->
+              <div class="box-body">
+                <a href="#">Announcement 1</a>
+              </div>
+              <div class="box-body">
+                <a href="#">Announcement 2</a>
+              </div>
+              <!-- /.box-body -->
             </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <a href="#">Announcement 1</a>
-            </div>
-            <div class="box-body">
-              <a href="#">Announcement 2</a>
-            </div>
-            <!-- /.box-body -->
+            <!-- /.box -->
           </div>
-          <!-- /.box -->
-        </div>
-    </div>
-    
+      </div>
     </section>
     
 @endsection
